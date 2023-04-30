a0e9ecde44b5c4f917a5f75c45aa546e0ea9a7d8
wmezadev <williameduardomeza@gmail.com>
2023-04-30 00:16:12 -0500

    create contacts and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
