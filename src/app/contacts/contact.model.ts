76dd67d6bb61423a5c540b3555f36344234128e0
wmezadev <williameduardomeza@gmail.com>
2023-04-30 00:45:59 -0500

    redner contact list

--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -4,9 +4,9 @@ export class Contact {
     public email: string;
     public phone: string;
     public imageUrl: string;
-    public group: Contact[];
+    public group: null | Contact[];
   
-    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group: Contact[]) {
+    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group: null) {
       this.id = id;
       this.name = name;
       this.email = email;
