36fc1b456feccaf6c68ac1a0264fbc8c5e789017
wmezadev <williameduardomeza@gmail.com>
2023-05-06 23:44:51 -0500

    Using Event Emitters and Inputs to pass Data between components

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -7,28 +7,16 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  contacts: Contact[] = [
-    new Contact(
-      '1',
-      'R. Kent Jackson',
-      'jacksonk@byui.edu',
-      '208-496-3771',
-      '../../assets/images/jacksonk.jpg',
-      null
-    ),
-    new Contact(
-      '2',
-      'Rex Barzee',
-      'barzeer@byui.edu',
-      '208-496-3768',
-      '../../assets/images/barzeer.jpg',
-      null
-    )
-  ];
+  @Input() contacts: Contact[] = [];
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
   }
 
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './contact.model';
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null
+    )
+  ];
+  selectedContact!: Contact;
 
+  ngOnInit() {
+  }
 }
