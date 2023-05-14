7cbc61ce10fccb8267c7faa3a22e7d1ab5080d04
wmezadev <williameduardomeza@gmail.com>
2023-05-13 23:19:59 -0500

    Implements Document Comps from downloaded files

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-detail',
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.css']
 })
 export class DocumentDetailComponent {
-
+  @Input() document!: Document;
 }
