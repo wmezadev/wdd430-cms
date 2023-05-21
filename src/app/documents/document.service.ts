04b3d4138b3e4feeedd1b16625a4b71d3fb36fc5
wmezadev <williameduardomeza@gmail.com>
2023-05-20 23:08:27 -0500

    feat: Create document service and Mock data

---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  documents: Document[] = [];
+  documentSelectedEvent = new EventEmitter<Document>();
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document | null {
+    return this.documents.find(document => document.id === id) || null;
+  }
+}
