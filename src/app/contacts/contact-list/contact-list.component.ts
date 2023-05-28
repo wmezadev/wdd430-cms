ff56a758207e9029ebcac925c722bd0f2ca5a87b
wmezadev <williameduardomeza@gmail.com>
2023-05-27 23:55:28 -0500

    feat: add Contact Delete methods in service and suscribe ContactList Comp

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -16,6 +16,9 @@ export class ContactListComponent implements OnInit {
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
+    this.contactService.contactChangedEvent.subscribe((contacts: Contact[]) => {
+      this.contacts = contacts;
+    });
   }
 
 }
--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -8,6 +8,7 @@ import { MOCKCONTACTS } from './MOCKCONTACTS';
 export class ContactService {
   contacts: Contact[] = [];
   contactSelectedEvent = new EventEmitter<Contact>();
+  contactChangedEvent: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
 
   constructor() {
     this.contacts = MOCKCONTACTS;
@@ -22,6 +23,14 @@ export class ContactService {
   }
 
   deleteContact(contact: Contact): void {
-    console.log("Attemp to delete " + contact.name)
+    if (!contact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(contact);
+    if (pos < 0) {
+      return;
+    }
+    this.contacts.splice(pos, 1);
+    this.contactChangedEvent.emit(this.contacts.slice());
   }
 }
