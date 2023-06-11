d0d55f218ac6511cf268090d26944d46793e3634
wmezadev <williameduardomeza@gmail.com>
2023-06-10 20:42:31 -0500

    feat: Add DocumentEdit Form logic

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
@@ -7,20 +10,53 @@ import { Document } from '../document.model';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-  originalDocument!: Document;
+  originalDocument!: Document | null;
   document: Document = new Document('', '', '', '', []);
   editMode: boolean = false;
 
-  constructor() {
-  }
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
   }
-  
-  onCancel (): void { }
 
-  onSubmit (f: any): void {
-    console.log(f)
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
+
+  onSubmit (form: NgForm): void {
+    const values = form.value;
+    // Assign form values to newDocument properties
+    const newDocument = new Document(
+      this.document.id, 
+      values.name, 
+      values.description, 
+      values.url, 
+      []
+    );    
+
+    if (this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+
+    this.router.navigate(['/documents']);
   }
 }
