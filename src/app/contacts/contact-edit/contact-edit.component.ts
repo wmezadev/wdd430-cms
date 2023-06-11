74f6331d066703d2fe50fd1837840571bac8f0e1
wmezadev <williameduardomeza@gmail.com>
2023-06-10 20:59:34 -0500

    feat: Add basic functionality for ContactEdit Form

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ContactService } from '../contact.service';
+import { Contact } from '../contact.model';
+import { group } from '@angular/animations';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrls: ['./contact-edit.component.css']
 })
-export class ContactEditComponent {
-  groupContacts: any;
-  onRemoveItem (i: any): void { }
-  onCancel (): void { }
+export class ContactEditComponent implements OnInit {
+  originalContact!: Contact | null;
+  contact: Contact = new Contact('', '', '', '', '', null);
+  editMode = false;
+  groupContacts: Contact[] = [];
+
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalContact = this.contactService.getContact(id);
+      if (!this.originalContact) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    const values = form.value;
+    const newContact = new Contact(
+      `${Math.floor(Math.random() * 1000) + 1}`, // random string
+      values.name,
+      values.email,
+      values.phone,
+      '', // imageUrl pending
+      null
+    );
+
+    if (this.editMode && this.originalContact) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+
+    this.router.navigate(['/contacts']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
+
+  onRemoveItem(i: any) {}
 }
