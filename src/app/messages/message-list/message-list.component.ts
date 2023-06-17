bc24216cb293f063a8391ba86ac45a76563248f4
wmezadev <williameduardomeza@gmail.com>
2023-06-17 01:45:37 -0500

    feat: Add Firebase Integration for Messages

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -12,7 +12,7 @@ export class MessageListComponent implements OnInit {
   constructor(private messageService: MessageService) {}
 
   ngOnInit() {
-    this.messages = this.messageService.getMessages();
+    //this.messages = this.messageService.getMessages();
 
     this.messageService.messageChangedEvent.subscribe((messages: Message[]) => {
       this.messages = messages;
--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
 
@@ -6,20 +7,61 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
   providedIn: 'root'
 })
 export class MessageService {
+  messagesUrl = `${import.meta.env['NG_APP_FIREBASE_URL']}/messages.json`;
   messages: Message[] = [];
   messageChangedEvent = new EventEmitter<Message[]>();
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.messages = MOCKMESSAGES;
+    this.getMessages();
+  }
+  
+  storeMessages() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const messagesString = JSON.stringify(this.messages);
+    this.http.put(this.messagesUrl, messagesString, { headers })
+    .subscribe({
+      next: () => {
+        this.messageChangedEvent.next(this.messages.slice());
+      },
+      error: (error: any) => {
+        console.error('Error storing messages:', error);
+      }
+    });
   }
 
   addMessage(message: Message) {
-    this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+    if (!message) {
+      return;
+    }
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const newDocument = JSON.parse(JSON.stringify(message));
+    this.http.post<{ name: string }>(
+      this.messagesUrl,
+      newDocument,
+      { headers }
+    ).subscribe({
+      next: (responseData) => {
+        this.messages.push(message);
+        this.storeMessages();
+      },
+      error: (error: any) => {
+        console.error('Error adding message:', error);
+      }
+    });
   }
 
-  getMessages(): Message[] {
-    return this.messages.slice();
+  getMessages() {
+    this.http.get<Message[]>(this.messagesUrl)
+      .subscribe({
+        next: (messages: Message[] ) => {
+          this.messages = messages;
+          this.messageChangedEvent.next(this.messages.slice());
+        },
+        error: (error: any) => {
+          console.error('An error occurred:', error);
+        }
+      });
   }
 
   getMessage(id: string): Message | null {
