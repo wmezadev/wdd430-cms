a13b785c702ca6cda31848472bac103342dba642
wmezadev <williameduardomeza@gmail.com>
2023-07-02 00:22:22 -0500

    fix: load contacts when messages are loading

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
+import { ContactService } from 'src/app/contacts/contact.service';
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
@@ -11,9 +12,10 @@ export class MessageListComponent implements OnInit {
   messages: Message[] = [];
   subscription: Subscription = new Subscription();
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private contactService: ContactService) {}
 
   ngOnInit() {
+    this.contactService.getContacts();
     this.messageService.getMessages();
     this.subscription = this.messageService.messageListChangedEvent.subscribe(
       (messages: Message[]) => {
