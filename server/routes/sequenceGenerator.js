aaebccefbcb6a95c3c317b0720bd720d3dc539ef
wmezadev <williameduardomeza@gmail.com>
2023-07-01 23:23:52 -0500

    fixes: sequencesGenerator

--- server/routes/sequenceGenerator.js
+++ server/routes/sequenceGenerator.js
@@ -6,56 +6,53 @@ let maxContactId;
 let sequenceId = null;
 
 function SequenceGenerator() {
-  return Sequence.findOne()
-    .then(sequence => {
+  Sequence.findOne()
+    .exec()
+    .then((sequence) => {
       sequenceId = sequence._id;
       maxDocumentId = sequence.maxDocumentId;
       maxMessageId = sequence.maxMessageId;
       maxContactId = sequence.maxContactId;
     })
-    .catch(err => {
-      return res.status(500).json({
-        title: 'An error occurred',
-        error: err
+    .catch((err) => {
+     console.error({
+        title: "An error occurred",
+        error: err,
       });
     });
 }
-
-
-SequenceGenerator.prototype.nextId = function(collectionType) {
-
+SequenceGenerator.prototype.nextId =  function (collectionType) {
   var updateObject = {};
   var nextId;
 
   switch (collectionType) {
-    case 'documents':
+    case "documents":
       maxDocumentId++;
-      updateObject = {maxDocumentId: maxDocumentId};
+      updateObject = { maxDocumentId: maxDocumentId };
       nextId = maxDocumentId;
       break;
-    case 'messages':
+    case "messages":
       maxMessageId++;
-      updateObject = {maxMessageId: maxMessageId};
+      updateObject = { maxMessageId: maxMessageId };
       nextId = maxMessageId;
       break;
-    case 'contacts':
+    case "contacts":
       maxContactId++;
-      updateObject = {maxContactId: maxContactId};
+      updateObject = { maxContactId: maxContactId };
       nextId = maxContactId;
       break;
     default:
       return -1;
   }
-
-  Sequence.update({_id: sequenceId}, {$set: updateObject},
-    function(err) {
-      if (err) {
-        console.log("nextId error = " + err);
-        return null
-      }
+  Sequence.updateOne({ _id: sequenceId }, { $set: updateObject })
+  .then(result => result)
+  .catch((err) => {
+   console.error({
+      title: "An error occurred",
+      error: err,
     });
-
+  });
   return nextId;
-}
+};
 
 module.exports = new SequenceGenerator();
